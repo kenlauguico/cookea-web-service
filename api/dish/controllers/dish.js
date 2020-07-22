f7202f6dd1129e030c42acec158e24f25f72edfa
Ken Lauguico <me@kenlauguico.com>
2020-07-22 03:22:27 -0700

    modified endpoints, dish/update wip

--- api/dish/controllers/dish.js
+++ api/dish/controllers/dish.js
@@ -1,8 +1,46 @@
 'use strict';
 
+const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
+
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  /**
+   * Update a dish.
+   *
+   * @return {Object}
+   */
+
+  // async update(ctx) {
+  //   const { dishId } = ctx.params;
+    
+  //   if (dishId) {
+
+  //     console.log("Using dishId: ", dishId)
+
+  //     const { user, times, stage } = ctx.request.body
+
+  //     console.log("User: "+user+", times: "+user+", stage: "+stage)
+
+  //     if (user && times && stage) {
+  //       console.log("It's time.")
+  //     }
+
+  //   }
+
+  //   // let entity;
+  //   // if (ctx.is('multipart')) {
+  //   //   const { data, files } = parseMultipartData(ctx);
+  //   //   entity = await strapi.services.restaurant.update({ id }, data, {
+  //   //     files,
+  //   //   });
+  //   // } else {
+  //   //   entity = await strapi.services.restaurant.update({ id }, ctx.request.body);
+  //   // }
+
+  //   // return sanitizeEntity(entity, { model: strapi.models.restaurant });
+  // },
+};
